8f203d923db3bf73fc390f0773696b17de675201
Anwar Choukah <anwarchoukah@gmail.com>
2015-12-01 13:26:33 +0000

    First Commit

--- site/config/config.php
+++ site/config/config.php
@@ -13,9 +13,9 @@ It is not permitted to run a public website without a
 valid license key. Please read the End User License Agreement
 for more information: http://getkirby.com/license
 
-*/
+ */
 
-c::set('license', 'put your license key here');
+c::set('license', 'de32cee8c5ea3ba138f9942329fc2827');
 
 /*
 
@@ -27,4 +27,4 @@ By default you don't have to configure anything to
 make Kirby work. For more fine-grained configuration
 of the system, please check out http://getkirby.com/docs/advanced/options
 
-*/
+ */
