975c91b7b4858220ee71bb06595d6a021ddfd0cf
Anwar Choukah <anwarchoukah@gmail.com>
2015-12-01 15:56:12 +0000

    Turning into css in progress
    Partials:
    
    _fonts.scss
    _header.scss
    _links.scss
    _original.scss
    _reset.scss
    _site.scss
    _variables.scss
    
    Changed fonts to Rubik and Roboto Mono (Google fonts)

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -5,18 +5,20 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-  <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
-  <meta name="description" content="<?php echo $site->description()->html() ?>">
-  <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
+  <title><?php echo $site->title()->html()?> | <?php echo $page->title()->html()?></title>
+  <meta name="description" content="<?php echo $site->description()->html()?>">
+  <meta name="keywords" content="<?php echo $site->keywords()->html()?>">
 
-  <?php echo css('assets/css/main.css') ?>
+  <link href='https://fonts.googleapis.com/css?family=Rubik:300,500,700,300italic,500italic|Roboto+Mono:500' rel='stylesheet' type='text/css'>
+
+<?php echo css('assets/css/main.css')?>
 
 </head>
 <body>
 
   <header class="header cf" role="banner">
-    <a class="logo" href="<?php echo url() ?>">
-      <img src="<?php echo url('assets/images/logo.svg') ?>" alt="<?php echo $site->title()->html() ?>" />
+    <a class="logo" href="<?php echo url()?>">
+      <img src="<?php echo url('assets/images/logo.svg')?>" alt="<?php echo $site->title()->html()?>" />
     </a>
-    <?php snippet('menu') ?>
+<?php snippet('menu')?>
   </header>
