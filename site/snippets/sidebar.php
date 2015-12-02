5db5e1d7a9304a0bad96528ec6eeccbc73e3037f
Anwar Choukah <anwarchoukah@gmail.com>
2015-12-02 21:44:30 +0000

    CSS and Content update
    - Header styles: using 1.125 Major Second from http://type-scale.com/
    - Clean up - fixed header, removed padding/margin from body
    - Inboard styling

--- site/snippets/sidebar.php
+++ site/snippets/sidebar.php
@@ -1,8 +1,32 @@
 <aside>
-  <h2>Hello</h2>
+  <h1>Meanwhile, elsewhere</h1>
   <ul>
-    <li class"sidebar-item">Hello</li>
-    <li class"sidebar-item">Hello</li>
-    <li class"sidebar-item">Hello</li>
+    <li class"sidebar-item pinboard">
+      <h2>Latest pins on Pinboard.in</h2>
+        <script language="javascript" src="http://pinboard.in//widgets/v1/linkroll/?user=Garbad&count=5">
+        </script>
+        <a href="https://pinboard.in/u:garbad/" title="More of Anwar Choukah's bookmarks on Pinboard.in" target="_blank">More bookmarks</a>
+    </li>
+    <li class"sidebar-item instagram">
+      <h2>Latest photos on Instagram</h2>
+      <!-- SnapWidget -->
+      <script src="http://snapwidget.com/js/snapwidget.js"></script>
+      <iframe src="http://snapwidget.com/in/?u=Z2FyYmFkfGlufDEyNXwzfDJ8fG5vfDV8bm9uZXxvblN0YXJ0fG5vfHllcw==&ve=220715" title="Instagram Widget" class="snapwidget-widget" allowTransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:100%;"></iframe>
+      <a href="http://instagram.com/garbad" title="Anwar Choukah on Instagram" target="_blank">More on Instagram</a>
+    </li>
+    <li class"sidebar-item">
+      <a class="twitter-timeline"
+        width="300"
+        height="500"
+        link-color="#c00"
+        data-chrome="noheader nofooter noborders transparent"
+        data-tweet-limit="5"
+        data-dnt="true"
+        href="https://twitter.com/achoukah"
+        data-widget-id="254912743650496514">Tweets by @achoukah</a>
+
+      <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+</script>
+    </li>
   </ul>
 </aside>
