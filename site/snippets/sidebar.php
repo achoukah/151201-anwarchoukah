1884e5421d7141fe16bddfd76715ab73eefac9af
Anwar Choukah <anwarchoukah@gmail.com>
2015-12-05 13:08:17 +0000

    Sidebar links etc

--- site/snippets/sidebar.php
+++ site/snippets/sidebar.php
@@ -2,28 +2,29 @@
   <h1>Meanwhile, elsewhere</h1>
   <ul>
     <li class"sidebar-item pinboard">
-      <h2>Latest pins on Pinboard.in</h2>
+      <h4>On Pinboard.in</h4>
         <script language="javascript" src="http://pinboard.in//widgets/v1/linkroll/?user=Garbad&count=6">
         </script>
-        <a href="https://pinboard.in/u:garbad/" title="More of Anwar Choukah's bookmarks on Pinboard.in" target="_blank">More bookmarks</a>
+        <!-- <a href="https://pinboard.in/u:garbad/" title="More of Anwar Choukah's bookmarks on Pinboard.in" target="_blank">More bookmarks</a> -->
     </li>
     <li class"sidebar-item instagram">
-      <h2>Latest photos on Instagram</h2>
+      <h4>On Instagram</h4>
       <!-- SnapWidget -->
 
       <script src="http://snapwidget.com/js/snapwidget.js"></script>
       <iframe src="http://snapwidget.com/in/?u=Z2FyYmFkfGlufDY0fDN8M3x8bm98NXxub25lfG9uU3RhcnR8bm98eWVz&ve=051215" title="Instagram Widget" class="snapwidget-widget" allowTransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:100%;"></iframe>
 
 
-      <a href="http://instagram.com/garbad" title="Anwar Choukah on Instagram" target="_blank">More on Instagram</a>
+      <!-- <a href="http://instagram.com/garbad" title="Anwar Choukah on Instagram" target="_blank">More on Instagram</a> -->
     </li>
     <li class"sidebar-item twitter">
+    <h4>On Twitter</h4>
       <a class="twitter-timeline"
         width="520"
         height="500"
         link-color="#c00"
         data-chrome="noheader nofooter noborders transparent"
-        data-tweet-limit="5"
+        data-tweet-limit="4"
         data-dnt="true"
         href="https://twitter.com/achoukah"
         data-widget-id="254912743650496514">Tweets by @achoukah</a>
