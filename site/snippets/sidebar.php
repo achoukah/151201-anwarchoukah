1c29281f7e17396374fdd1cf385d9749536730b3
Anwar Choukah <anwarchoukah@gmail.com>
2015-12-14 22:27:38 +0000

    Sidebar/aside fixes, <code> preview
    looks better now.

--- site/snippets/sidebar.php
+++ site/snippets/sidebar.php
@@ -1,22 +1,22 @@
 <aside>
   <h1>Meanwhile, elsewhere</h1>
   <ul>
-    <li class"sidebar-item pinboard">
+    <li class="sidebar-item pinboard">
       <h4>On Pinboard.in</h4>
         <script language="javascript" src="http://pinboard.in//widgets/v1/linkroll/?user=Garbad&count=6">
         </script>
         <a class="more" href="https://pinboard.in/u:garbad/" title="More of Anwar Choukah's bookmarks on Pinboard.in" target="_blank">Public bookmarks on Pinboard.in</a>
     </li>
-    <li class"sidebar-item instagram">
+    <li class="sidebar-item instagram">
       <h4>On Instagram</h4>
       <!-- SnapWidget -->
-<script src="http://snapwidget.com/js/snapwidget.js"></script>
-<iframe src="http://snapwidget.com/in/?u=Z2FyYmFkfGlufDEyNXwyfDN8fG5vfDh8bm9uZXxvblN0YXJ0fG5vfHllcw==&ve=121215" title="Instagram Widget" class="snapwidget-widget" allowTransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:100%;"></iframe>
+      <script src="http://snapwidget.com/js/snapwidget.js"></script>
+      <iframe src="http://snapwidget.com/in/?u=Z2FyYmFkfGlufDEyNXwyfDN8fG5vfDh8bm9uZXxvblN0YXJ0fG5vfHllcw==&ve=121215" title="Instagram Widget" class="snapwidget-widget" allowTransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:100%;"></iframe>
 
 
       <a class="more" href="http://instagram.com/garbad" title="Anwar Choukah on Instagram" target="_blank">More on Instagram</a>
     </li>
-    <li class"sidebar-item twitter">
+    <li class="sidebar-item twitter">
     <h4>On Twitter</h4>
       <a class="twitter-timeline"
         width="520"
@@ -26,10 +26,12 @@
         data-tweet-limit="4"
         data-dnt="true"
         href="https://twitter.com/achoukah"
-        data-widget-id="254912743650496514">Tweets by @achoukah</a>
+        data-widget-id="643383875134210048">Tweets by @achoukah</a>
 
-      <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
-</script>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+        </script>
+
+
       <a class="more" href="http://twitter.com/achoukah" title="Anwar Choukah on Twitter" target="_blank">@achoukah on Twitter</a>
     </li>
   </ul>
