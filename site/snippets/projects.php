67072e9a195812ff32e167fac692149590e65bb2
Anwar Choukah <anwarchoukah@gmail.com>
2015-12-17 10:31:03 +0000

    Project index design

--- site/snippets/projects.php
+++ site/snippets/projects.php
@@ -1,12 +1,12 @@
 <h2>Latest projects</h2>
 
-<ul class="teaser cf">
+<ul class="teaser">
   <?php foreach(page('projects')->children()->visible()->limit(3) as $project): ?>
   <li>
     <h3><a href="<?php echo $project->url() ?>"><?php echo $project->title()->html() ?></a></h3>
-    <p><?php echo $project->text()->excerpt(80) ?> <a href="<?php echo $project->url() ?>">read&nbsp;more&nbsp;→</a></p>
+    <p><?php echo $project->text()->excerpt(80) ?> <a class="read-more" href="<?php echo $project->url() ?>">Read More</a></p>
     <?php if($image = $project->images()->sortBy('sort', 'asc')->first()): ?>
-    <a href="<?php echo $project->url() ?>">
+    <a href="<?php echo $project->url() ?>" class="project-thumb">
       <img src="<?php echo $image->url() ?>" alt="<?php echo $project->title()->html() ?>" >
     </a>
     <?php endif ?>
