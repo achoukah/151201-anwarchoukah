77d87f989f6879ff7d882b866fbdc2c6ae6d7e50
Anwar Choukah <anwarchoukah@gmail.com>
2015-12-22 13:20:17 +0000

    Remove subnav completely
    from site > menu.php

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -4,21 +4,6 @@
     <?php foreach($pages->visible() as $p): ?>
     <li>
       <a <?php e($p->isOpen(), ' class="active"') ?> href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
-
-      <!--
-
-      <?php if($p->hasVisibleChildren()): ?>
-      <ul class="submenu">
-        <?php foreach($p->children()->visible() as $p): ?>
-        <li>
-          <a href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
-        </li>
-        <?php endforeach ?>
-      </ul>
-      <?php endif ?>
-
-      -->
-
     </li>
     <?php endforeach ?>
   </ul>
