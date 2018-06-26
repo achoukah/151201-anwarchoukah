cdcc86487f00eeaa0eb74df7fe2af9adface11c6
Anwar Choukah <anwarchoukah@gmail.com>
2018-06-26 13:15:57 +0100

    CONTACT: Fine tune form, add validation

--- site/snippets/contactform.php
+++ site/snippets/contactform.php
@@ -1,55 +1,32 @@
-<?php
-
-  $form = new contactform(array(
-    'to'       => 'Anwar <anwarchoukah@gmail.com>',
-    'from'     => 'Contact Form <anwarchoukah@gmail.com>',
-    'subject'  => 'New contact form request',
-    'goto'     => $page->url() . '/status:thank-you'
-  ));
-?>
+<!-- CONTACT FORM -->
 
 <section id="contactform">
 
-  <?php if(param('status') == 'thank-you'): ?>
-
-  <h1>Thank you very much for your request</h1>
-  <p class="contactform-text">I will get in contact as soon as possible</p>
-
-  <?php else: ?>
-
-  <!-- <h1>Get in touch</h1> -->
 
   <form method="POST" action="https://formspree.io/choukah@me.com">
     <fieldset>
-
-      <?php if($form->isError('send')): ?>
-      <div class="contactform-alert">The email could not be sent. Please try again later.</div>
-      <?php elseif($form->isError()): ?>
-      <div class="contactform-alert">The form could not be submitted. Please fill in all fields correctly.</div>
-      <?php endif ?>
-
-      <div class="contactform-field<?php if($form->isError('name')) echo ' error' ?>">
-        <label class="contactform-label" for="contactform-name">Name <?php if($form->isRequired('name')) echo '<span>*</span>' ?> <?php if($form->isError('name')): ?><small>Please enter a name</small><?php endif ?></label>
-        <input class="contactform-input" type="text" id="contactform-name" name="name" value="<?php echo $form->htmlValue('name') ?>" />
+      <div class="contactform-field">
+        <label class="contactform-label" for="contactform-name">Name</label>
+        <input class="contactform-input" name="name" placeholder="Your name" type="text" id="contactform-name" required>
       </div>
 
-      <div class="contactform-field<?php if($form->isError('email')) echo ' error' ?>">
-        <label class="contactform-label" for="contactform-email">Email adresse <?php if($form->isRequired('email')) echo '<span>*</span>' ?> <?php if($form->isError('email')): ?><small>Please enter a valid email address</small><?php endif ?></label>
-        <input class="contactform-input" type="text" id="contactform-email" name="email" value="<?php echo $form->htmlValue('email') ?>" />
+      <div class="contactform-field">
+        <label class="contactform-label" for="contactform-email">Email address</label>
+        <input class="contactform-input" name="email" placeholder="Your email" type="email" id="contactform-email" required>
       </div>
 
-      <div class="contactform-field<?php if($form->isError('text')) echo ' error' ?>">
-        <label class="contactform-label" for="contactform-text">Text <?php if($form->isRequired('text')) echo '<span>*</span>' ?> <?php if($form->isError('text')): ?><small>Please enter your text</small><?php endif ?></label>
-        <textarea class="contactform-input" name="text" id="contactform-text"><?php echo $form->htmlValue('text') ?></textarea>
+      <div class="contactform-field">
+        <label class="contactform-label" for="contactform-text">Message</label>
+        <textarea class="contactform-input" name="message" placeholder="Your message" id="contactform-text" required></textarea>
       </div>
-
-      <p class="contactform-help">All fields with * are required</p>
 
       <input class="contactform-button" type="submit" name="submit" value="Send" />
 
     </fieldset>
   </form>
 
-  <?php endif ?>
 
 </section>
+
+
+<!-- END CONTACT FORM -->
